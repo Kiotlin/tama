779534ce6bc0f1c3fecf990565d0344d652213e3
ri1ken <719401217@qq.com>
2022-09-22 22:33:15 +0900

    update seo info

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,38 @@ import Head from "next/head";
 import "../styles/reset.css";
 
 export default function TAMA({ Component, pageProps }) {
+  const meta = {
+    title: "TAMA",
+    description: "Beautiful 3d glass material balls in browser.",
+    image: "https://tama.vercel.app/logo.png",
+  };
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>TAMA</title>
         <link rel="icon" href="/logo.svg" />
+        <meta name="author" content="ri1ken" />
+        <link rel="canonical" href="https://tama.vercel.app" />
+        <meta name="title" content={meta.title} />
+        <meta property="description" content={meta.description} />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:url" content="https://tama.vercel.app" />
+        <meta
+          property="og:image"
+          content={meta.image || "https://tama.vercel.app/logo.png"}
+        />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="@Kiokh_" />
+        <meta property="twitter:title" content={meta.title} />
+        <meta property="twitter:description" content={meta.description} />
+        <meta property="twitter:url" content="https://tama.vercel.app" />
+        <meta
+          property="twitter:image"
+          content={meta.image || "https://tama.vercel.app/logo.png"}
+        />
       </Head>
       <Component {...pageProps} />
       <div className="author">
